387eabc0039d3bd528db86eff6e10e9a0922c7f4
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-07-21 13:29:28 +0400

    fix: add articles.index route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\{PageController, ArticleController};
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +20,4 @@ Route::get('/', function () {
 
 Route::get('about', [PageController::class, 'about']);
 
+Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
