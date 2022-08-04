5262b1199d9145c3b8cc552cdc7f1c2ad726e39c
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-08-04 17:33:52 +0400

    fix: add delete link

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -4,4 +4,5 @@
     <h1>{{ $article->name }}</h1>
     <div>{{ $article->body }}</div>
     <p><a href="{{ route('articles.edit', $article->id) }}">Редактировать</a></p>
+    <p><a href="{{ route('articles.destroy', $article->id) }}" data-method="delete" rel="nofollow">Удалить</a></p>
 @endsection
