4ee112bc3e39e1eb2ef35bf4e754c5afd105f4b7
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-08-05 10:50:42 +0400

    feat: create resource controller instead other controllers

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,25 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Article;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        $articles = Article::paginate();
+        $articles = Article::paginate(5);
 
         return view('article.index', compact('articles'));
     }
 
-    public function show($id)
-    {
-        $article = Article::findOrFail($id);
-
-        return view('article.show', compact('article'));
-    }
-
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         $article = new Article();
@@ -28,33 +31,54 @@ class ArticleController extends Controller
         return view('article.create', compact('article'));
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $data = $request->validate([
             'name' => ['required', 'unique:articles'],
-            'body' => ['required', 'min:100']
+            'body' => ['required', 'min:100'],
         ]);
 
         $article = new Article();
-
-        $article->fill($data)->save();
-
+        $article->fill($data)
+            ->save();
         $request->session()->flash('success', 'Статья успешно добавлена');
 
         return redirect()->route('articles.index');
     }
 
-    public function edit($id)
+    /**
+     * Display the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
     {
-        $article = Article::findOrFail($id);
+        return view('article.show', compact('article'));
+    }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
         return view('article.edit', compact('article'));
     }
 
-    public function update(Request $request, $id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
     {
-        $article = Article::findOrFail($id);
-        $data = $this->validate($request, [
+        $data = $request->validate([
             'name' => 'required|unique:articles,name,' . $article->id,
             'body' => 'required|min:100',
         ]);
@@ -62,15 +86,19 @@ class ArticleController extends Controller
         $article->fill($data)
             ->save();
 
-        return redirect()
-            ->route('articles.index');
+        $request->session()->flash('success', 'Статья успешно обновлена');
+
+        return redirect()->route('articles.index');
     }
 
-    public function destroy(Request $request, $id)
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, Article $article)
     {
-        $article = Article::find($id);
-
-        if($article) {
+        if ($article) {
             $article->delete();
         }
 
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -10,4 +10,5 @@
     <h2><a href="{{ route('articles.show', $article->id) }}">{{ $article->name }}</a></h2>
     <div>{{ Str::limit($article->body, 200) }}</div>
 @endforeach
+{{ $articles->links() }}
 @endsection
